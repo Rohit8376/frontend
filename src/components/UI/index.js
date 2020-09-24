6803b94a43dbac11aaa0b21304171ff0b86f610a
rohit pandey <1080pandey@gmail.com>
2020-09-24 14:24:57 +0530

    sencond adition

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -13,8 +13,8 @@ const Input=(props)=> {
         <>
         
         <Form.Group >
-               <Form.Label>{props.label}</Form.Label>
-              <Form.Control type={props.type} placeholder={props.placeholder} value={props.value} onChange={props.onChange}  />
+              <Form.Label>{props.label}</Form.Label>
+              <Form.Control type={props.type} placeholder={props.placeholder} value={props.value} onChange={props.onChange} name={props.name}  />
               <Form.Text className="text-muted">
                {props.errorMessage}
               </Form.Text>
--- src/containers/add_car/index.js
+++ src/containers/add_car/index.js
@@ -10,44 +10,69 @@ import { Container, Form, Button, Row, Col } from "react-bootstrap";
 class AddCar extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       car_name: null,
       car_img: null,
-      specifications: null,
       specdtl: null,
-      icon: null,
-      spec: null,
+
+      inputfield: [{ icon: null, spec: null }],
     };
   }
+
+  HandleChangeInput = (index, event) => {
+    if (event.target.name === "icon") {
+      const values = [...this.state.inputfield];
+      values[index][event.target.name] = event.target.files[0];
+      this.setState({ inputfield: values });
+    } else {
+      const values = [...this.state.inputfield];
+      values[index][event.target.name] = event.target.value;
+      this.setState({ inputfield: values });
+    }
+  };
+
+  handleaddfield = () => {
+    const Inputfiled = [...this.state.inputfield];
+    Inputfiled.push({ icon: null, spec: null });
+    this.setState({ inputfield: Inputfiled });
+  };
+
+  handleRemovefield = (index) => {
+    console.log(index);
+    const values = [...this.state.inputfield];
+    values.splice(index, 1);
+    this.setState({ inputfield: values });
+  };
+
   signup = (e) => {
+
     e.preventDefault();
 
-    //  const spec = [ {icon:this.state.icon,spec:this.state.spec},
-    //                 {icon:this.state.icon,spec:this.state.spec},
-    //                 {icon:this.state.icon,spec:this.state.spec}
-
-    //                ]
-
     const formData = new FormData();
+
+    this.state.inputfield.forEach((file) => {
+      formData.append("spec", file.icon, file.icon.name);
+    });
+
+    this.state.inputfield.forEach((file) => {
+      formData.append("spec", file.spec);
+    });
 
     formData.append("name", this.state.car_name);
     formData.append("carimage", this.state.car_img);
     formData.append("specdtl", this.state.specdtl);
-    // formData.append("spec", spec)
-    formData.append("spec", this.state.icon);
-    formData.append("spec", this.state.spec);
-
-    console.log(...formData);
     axios
       .post("https://myapp-backend.herokuapp.com/api/admin/create", formData)
       .then((err, res) => {
         if (res) {
-          window.location.reload(false);
+          console.log("uploaded");
         }
       });
   };
 
+
+
+  
   render() {
     return (
       <Layout>
@@ -57,24 +82,24 @@ class AddCar extends React.Component {
               <Form
                 onSubmit={(e) => this.signup(e)}
                 style={{ border: "1px solid gray", padding: "10px" }}
-                autocomplete="off"
+                autoComplete="off"
               >
                 <Row>
-                  <Col md={12}>
+                  <Col md={12} sm={12}>
                     <Input
                       label="Car Name"
                       placeholder="Car Name"
                       type="text"
-                      autocomplete="off"
+                      autoComplete="off"
                       onChange={(e) => {
                         this.setState({ car_name: e.target.value });
                       }}
                     />
                   </Col>
 
-                  <Col md={12}>
+                  <Col md={12} sm={12}>
                     <Input
-                      autocomplete="off"
+                      autoComplete="off"
                       label="Car Image"
                       type="file"
                       onChange={(e) => {
@@ -85,36 +110,64 @@ class AddCar extends React.Component {
                   <hr></hr>
                 </Row>
 
-                <Row>
-                  <Col md={6} sm={6}>
-                    <Input
-                      autocomplete="off"
-                      label="Icon"
-                      type="file"
-                      onChange={(e) => {
-                        this.setState({ icon: e.target.files[0] });
-                      }}
-                    />
-                  </Col>
-                  <Col md={6} sm={6}>
-                    <Input
-                      autocomplete="off"
-                      label="Specification"
-                      placeholder="Car Name"
-                      type="text"
-                      onChange={(e) => {
-                        this.setState({ spec: e.target.value });
-                      }}
-                    />
-                  </Col>
-                  <hr></hr>
-                </Row>
+                {this.state.inputfield.map((inputfield, index) => (
+                  <Row
+                    key={index}
+                    style={{ backgroundColor: "lightgrey", margin: "10px" }}
+                  >
+                    <Col md={4} sm={5}>
+                      <Input
+                        label="Icon"
+                        type="file"
+                        name="icon"
+                        value={this.state.inputfield.firstname}
+                        onChange={(event) =>
+                          this.HandleChangeInput(index, event)
+                        }
+                      />
+                    </Col>
+                    <Col md={4} sm={4}>
+                      <Input
+                        label="Specification"
+                        name="spec"
+                        type="text"
+                        // value={this.state.inputfield.firstname}
+                        onChange={(event) =>
+                          this.HandleChangeInput(index, event)
+                        }
+                      />
+                    </Col>
+                    <Button
+                      onClick={() => this.handleRemovefield(index)}
+                      style={{ marginTop: "30px", marginBottom: "20px" }}
+                    >
+                      remove
+                    </Button>
+                    <hr></hr>
+                  </Row>
+                ))}
+
+                <Button
+                  onClick={() => this.handleaddfield()}
+                  style={{
+                    marginTop: "30px",
+                    marginBottom: "20px",
+                    marginLeft: "30px",
+                  }}
+                >
+                  add Specification
+                </Button>
 
                 <Input
                   label="Specification file"
                   type="file"
                   onChange={(e) => {
                     this.setState({ specdtl: e.target.files[0] });
+                  }}
+                  style={{
+                    backgroundColor: "lightgrey",
+                    marginLeft: "30px",
+                    marginRight: "30px",
                   }}
                 />
 
@@ -131,3 +184,158 @@ class AddCar extends React.Component {
 }
 
 export default AddCar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import Layout from "../../components/Layout";
+// import Input from "../../components/UI";
+// import "antd/dist/antd.css";
+// import "../../App.css";
+// import axios from "axios";
+
+// import { Container, Form, Button, Row, Col } from "react-bootstrap";
+
+// class AddCar extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       car_name: null,
+//       car_img: null,
+//       specifications: null,
+//       specdtl: null,
+//       icon: null,
+//       spec: null,
+//     };
+//   }
+//   signup = (e) => {
+//     e.preventDefault();
+
+//     //  const spec = [ {icon:this.state.icon,spec:this.state.spec},
+//     //                 {icon:this.state.icon,spec:this.state.spec},
+//     //                 {icon:this.state.icon,spec:this.state.spec}
+
+//     //                ]
+
+//     const formData = new FormData();
+
+//     formData.append("name", this.state.car_name);
+//     formData.append("carimage", this.state.car_img);
+//     formData.append("specdtl", this.state.specdtl);
+//     // formData.append("spec", spec)
+//     formData.append("spec", this.state.icon);
+//     formData.append("spec", this.state.spec);
+
+//     console.log(...formData);
+//     axios
+//       .post("https://myapp-backend.herokuapp.com/api/admin/create", formData)
+//       .then((err, res) => {
+//         if (res) {
+//            window.location.href = "/signin";
+//         }
+//       });
+//   };
+
+//   render() {
+//     return (
+//       <Layout>
+//         <Container>
+//           <Row style={{ marginTop: "50px" }}>
+//             <Col md={{ span: 6, offset: 3 }}>
+//               <Form
+//                 onSubmit={(e) => this.signup(e)}
+//                 style={{ border: "1px solid gray", padding: "10px" }}
+//                 autocomplete="off"
+//               >
+//                 <Row>
+//                   <Col md={12}>
+//                     <Input
+//                       label="Car Name"
+//                       placeholder="Car Name"
+//                       type="text"
+//                       autocomplete="off"
+//                       onChange={(e) => {
+//                         this.setState({ car_name: e.target.value });
+//                       }}
+//                     />
+//                   </Col>
+
+//                   <Col md={12}>
+//                     <Input
+//                       autocomplete="off"
+//                       label="Car Image"
+//                       type="file"
+//                       onChange={(e) => {
+//                         this.setState({ car_img: e.target.files[0] });
+//                       }}
+//                     />
+//                   </Col>
+//                   <hr></hr>
+//                 </Row>
+
+//                 <Row>
+//                   <Col md={6} sm={6}>
+//                     <Input
+//                       autocomplete="off"
+//                       label="Icon"
+//                       type="file"
+//                       onChange={(e) => {
+//                         this.setState({ icon: e.target.files[0] });
+//                       }}
+//                     />
+//                   </Col>
+//                   <Col md={6} sm={6}>
+//                     <Input
+//                       autocomplete="off"
+//                       label="Specification"
+//                       placeholder="Car Name"
+//                       type="text"
+//                       onChange={(e) => {
+//                         this.setState({ spec: e.target.value });
+//                       }}
+//                     />
+//                   </Col>
+//                   <hr></hr>
+//                 </Row>
+
+//                 <Input
+//                   label="Specification file"
+//                   type="file"
+//                   onChange={(e) => {
+//                     this.setState({ specdtl: e.target.files[0] });
+//                   }}
+//                 />
+
+//                 <Button variant="primary" type="submit">
+//                   Submit
+//                 </Button>
+//               </Form>
+//             </Col>
+//           </Row>
+//         </Container>
+//       </Layout>
+//     );
+//   }
+// }
+
+// export default AddCar;
